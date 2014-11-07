04c77eac58d4a604819c55b0a27e4ad18bcfdaf6
Peter David Faria <zshift@gmail.com>
2014-11-07 14:00:23 -0800

    Changed return type of exercise19 for consistency.

--- src/learnrxjava/ComposableList.java
+++ src/learnrxjava/ComposableList.java
@@ -749,7 +749,7 @@ public class ComposableList<T> extends ArrayList<T> {
 
      This is a variation of the problem we solved earlier, where we retrieved the url of the boxart with a width of 150px. This time we'll use reduce() instead of filter() to retrieve the smallest box art in the boxarts list.
      */
-    public JSON exercise19() {
+    public ComposableList<JSON> exercise19() {
         ComposableList<MovieList> movieLists = ComposableList.of(
                 new MovieList(
                         "New Releases",
